9b94bee0643f66be363162ad9c43f51278b4f13f
MariaHelenaLB <mariahelenalimabarreto13@gmail.com>
2024-04-30 11:21:20 -0300

    Aula 116 (DoCheck).

--- meu-primeiro-projeto-latest/src/app/app.component.ts
+++ meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -19,12 +19,12 @@ import { LifeCycleComponent } from './components/life-cycle/life-cycle.component
 
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
+  selector: 'app-root',
+  standalone: true,
 
-    imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent, TemplateDeferrableViewsComponent, SignalComponent, PaiOuMaeComponent, AngularPipesComponent, ReactiveFormsComponent, TemplateDrivenFormsComponent, ContentComponent, HostElementsComponent, LifeCycleComponent],
+  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent, TemplateDeferrableViewsComponent, SignalComponent, PaiOuMaeComponent, AngularPipesComponent, ReactiveFormsComponent, TemplateDrivenFormsComponent, ContentComponent, HostElementsComponent, LifeCycleComponent],
 
-    template: `
+  template: `
 <!-- 
   <router-outlet></router-outlet>  
   <app-new-component /> 
@@ -54,15 +54,13 @@ import { LifeCycleComponent } from './components/life-cycle/life-cycle.component
   <h1>Curso de Angular</h1>
   <app-life-cycle [myNumber]="number" />
 
-  `,   
+  `,
 
 })
 export class AppComponent implements OnInit {
   public number = 1;
 
   ngOnInit(): void {
-    setInterval(() => {
-      this.number++;
-    }, 1000);
+   
   }
 }
--- meu-primeiro-projeto-latest/src/app/components/life-cycle/life-cycle.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/life-cycle/life-cycle.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, DoCheck, Input, OnChanges, OnInit, SimpleChanges, signal } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 
 @Component({
@@ -9,20 +9,32 @@ import { FormBuilder } from '@angular/forms';
   templateUrl: './life-cycle.component.html',
   styleUrl: './life-cycle.component.scss'
 })
-export class LifeCycleComponent implements OnChanges{
+export class LifeCycleComponent implements OnChanges, OnInit, DoCheck {
 
   @Input() public myNumber = 0;
 
+  public myText = signal('MH')
+
   //construtor ou inicializador
-  constructor(private fb: FormBuilder){}
+  constructor(private fb: FormBuilder) { }
 
   //changes detections
   ngOnChanges(changes: SimpleChanges): void {
     //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
     //Add '${implements OnChanges}' to the class.
     console.log('ngOnChanges', changes);
-    if (changes['myNumber'].previousValue === 2) {
-      alert ('Deu certo!');
-    }
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log('ngOnInit');
+  }
+
+  ngDoCheck(): void {
+    //Called every time that the input properties of a component or a directive are checked. Use it to extend change detection by performing a custom check.
+    //Add 'implements DoCheck' to the class.
+    console.log('ngDoCheck');
   }
 }
