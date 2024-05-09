c1a365594154248e953e4c3cca7bc40e2b9ad373
MariaHelenaLB <mariahelenalimabarreto13@gmail.com>
2024-05-09 14:20:14 -0300

    Aula 145 (Lazy load component).

--- meu-primeiro-projeto-latest/src/app/app.routes.ts
+++ meu-primeiro-projeto-latest/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 
-//components pages
+/*//components pages
 import { HomeComponent } from './pages/home/home.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
 import { ServicosPrestadosComponent } from './pages/servicos-prestados/servicos-prestados.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';*/
 
 //localhost:4200/
 export const routes: Routes = [
@@ -14,48 +14,53 @@ export const routes: Routes = [
             {
                 path: '',
                 title: 'Home da página',
-                component: HomeComponent,
+                loadComponent: () =>
+                    import('./pages/home/home.component'),
             },
 
             {
                 path: 'sobre',
                 title: 'Sobre da página',
-                component: SobreComponent,
+                loadComponent: () =>
+                    import('./pages/sobre/sobre.component'),
             },
 
             {
                 path: 'servicos/:id',
                 title: 'Servicos da página',
-                component: ServicosPrestadosComponent,
+                loadComponent: () =>
+                    import('./pages/servicos-prestados/servicos-prestados.component'),
             },
-        ]
+        ],
     },
 
+    /* 
     {
-        path: 'dashboard',
-        children: [
-            {
-                path: '',
-                title: 'Home da página',
-                component: HomeComponent,
-            },
-        
-            {
-                path: 'sobre',
-                title: 'Sobre da página',
-                component: SobreComponent,
-            },
-        
-            {
-                path: 'servicos/:id',
-                title: 'Servicos da página',
-                component: ServicosPrestadosComponent,
-            },
-        ]
-    },
+         path: 'dashboard',
+         children: [
+             {
+                 path: '',
+                 title: 'Home da página',
+                 component: HomeComponent,
+             },
+         
+             {
+                 path: 'sobre',
+                 title: 'Sobre da página',
+                 component: SobreComponent,
+             },
+         
+             {
+                 path: 'servicos/:id',
+                 title: 'Servicos da página',
+                 component: ServicosPrestadosComponent,
+             },
+         ],
+     },
+     */
 
     {
         path: '**',
-        component: NotFoundComponent,
+        loadComponent: () => import('./pages/not-found/not-found.component'),
     },
 ];
