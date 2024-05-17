a0bf8cc2160a9fd2ddb5f80d2b746553e6badea3
MariaHelenaLB <mariahelenalimabarreto13@gmail.com>
2024-05-17 11:53:04 -0300

    Aula 147 (Query Params).

--- meu-primeiro-projeto-latest/src/app/pages/curso.routes.ts
+++ meu-primeiro-projeto-latest/src/app/pages/curso.routes.ts
@@ -8,7 +8,31 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';*/
 
 //localhost:4200/
 export const cursoRoutes: Routes = [
-    {
+
+            {
+                path: '',
+                title: 'Home da página',
+                loadComponent: () =>
+                    import('./home/home.component'),
+            },
+
+            {
+                path: 'sobre',
+                title: 'Sobre da página',
+                loadComponent: () =>
+                    import('./sobre/sobre.component'),
+            },
+
+            {
+                path: 'servicos/:id',
+                title: 'Servicos da página',
+                loadComponent: () =>
+                    import('./servicos-prestados/servicos-prestados.component'),
+            },
+        ];
+
+/*  
+{
         path: '',
         children: [
             {
@@ -32,5 +56,6 @@ export const cursoRoutes: Routes = [
                     import('./servicos-prestados/servicos-prestados.component'),
             },
         ],
-    },
-];
+},
+*/
+
--- meu-primeiro-projeto-latest/src/app/pages/servicos-prestados/servicos-prestados.component.ts
+++ meu-primeiro-projeto-latest/src/app/pages/servicos-prestados/servicos-prestados.component.ts
@@ -16,12 +16,26 @@ export default class ServicosPrestadosComponent implements OnInit {
   public getId = signal<null | string>(null);
   @Input() set id(id: string) {
     this.getId.set(id);
-  }
+  };
 //dependendo da situação você pode usar o ngOnInit 
   ngOnInit(): void {
     console.log(this.#route.snapshot.params['id']);
     this.#route.params.subscribe((res) => console.log(res['id']));
 
+    console.log(this.#route.snapshot.queryParamMap.get('name'));
+    console.log(this.#route.snapshot.queryParamMap.get('age'));
+    
+    /*
+    **outras formas de pegar os dados com queryParam:**
+    this.#route.queryParamMap.subscribe((res) => console.log(res.get('name')));
+    this.#route.queryParamMap.subscribe((res) => console.log(res.get('age')));
 
-  }
-}
+    this.#route.queryParamMap.subscribe ({
+      next: (next) => {
+        console.log(next.get('name'));
+        console.log(next.get('age'));
+      },
+    });
+    */
+  };
+};
