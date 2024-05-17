8d2fdecadc187d829febba90f7e51a3cd27d708e
MariaHelenaLB <mariahelenalimabarreto13@gmail.com>
2024-05-17 12:04:28 -0300

    Aula 148 (Router Navigate).

--- meu-primeiro-projeto-latest/src/app/pages/servicos-prestados/servicos-prestados.component.ts
+++ meu-primeiro-projeto-latest/src/app/pages/servicos-prestados/servicos-prestados.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input, OnInit, inject, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-servicos-prestados',
@@ -12,30 +12,34 @@ import { ActivatedRoute } from '@angular/router';
 })
 export default class ServicosPrestadosComponent implements OnInit {
   #route = inject(ActivatedRoute);
+  #router = inject(Router); // # = private router
 
   public getId = signal<null | string>(null);
   @Input() set id(id: string) {
     this.getId.set(id);
   };
-//dependendo da situação você pode usar o ngOnInit 
+  //dependendo da situação você pode usar o ngOnInit 
   ngOnInit(): void {
     console.log(this.#route.snapshot.params['id']);
     this.#route.params.subscribe((res) => console.log(res['id']));
 
     console.log(this.#route.snapshot.queryParamMap.get('name'));
     console.log(this.#route.snapshot.queryParamMap.get('age'));
-    
-    /*
-    **outras formas de pegar os dados com queryParam:**
+
+
+    //**outras formas de pegar os dados com queryParam:**
     this.#route.queryParamMap.subscribe((res) => console.log(res.get('name')));
     this.#route.queryParamMap.subscribe((res) => console.log(res.get('age')));
 
-    this.#route.queryParamMap.subscribe ({
+    this.#route.queryParamMap.subscribe({
       next: (next) => {
         console.log(next.get('name'));
         console.log(next.get('age'));
       },
     });
-    */
+
+    //navigate faz com que a gente ande para as outras rotas
+    setTimeout(() => this.#router.navigate(['/curso']), 3000);
+
   };
 };
