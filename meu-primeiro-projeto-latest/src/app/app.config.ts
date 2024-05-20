8a768417c3db29c9e70e3b7ff75347d746a554fc
MariaHelenaLB <mariahelenalimabarreto13@gmail.com>
2024-05-20 15:00:58 -0300

    Aula 157 (ProvideImgixLoader).

--- meu-primeiro-projeto-latest/src/app/app.config.ts
+++ meu-primeiro-projeto-latest/src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig, LOCALE_ID } from '@angular/core';
+import { environment } from 'environments/environment';
 
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter, withComponentInputBinding, withRouterConfig } from '@angular/router';
 
 import { routes } from './app.routes';
 
-import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 
 //interceptor
@@ -14,6 +14,9 @@ import { httpInterceptor } from './interceptor/http.interceptor';
 
 //translate
 import { provideTranslate } from './app.translate';
+
+//img
+import { IMAGE_LOADER, ImageLoaderConfig, provideImgixLoader, registerLocaleData } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -24,6 +27,22 @@ export const appConfig: ApplicationConfig = {
     )),
     provideHttpClient(withInterceptors([httpInterceptor])),
     provideTranslate(),
+
+    //provideImgixLoader(environment.img),
+
+    /*
+    OUTRA FORMA DE TRABALHAR E CARREGAR IMAGENS
+    {
+      provide: IMAGE_LOADER, useValue: (config: ImageLoaderConfig) => {
+        const img = config.src.split('.');
+        const name = img.shift();
+        const type = img.pop(); //pop pega o último item do array
+        const width = config.width;
+        return `${environment.img}${name}-${width}w.${type}`;
+      },
+    },
+    */
+
     { provide: LOCALE_ID, useValue: 'pt-BR' },
   ],
 };
--- meu-primeiro-projeto-latest/src/environments/environment.homologation.ts
+++ meu-primeiro-projeto-latest/src/environments/environment.homologation.ts
@@ -1,4 +1,5 @@
 export const environment = {
     env: 'hom',
     apiTasks: 'https://us-central1-curso-de-angular-api.cloudfunctions.net/app/tasks/',
+    img: 'http://localhost:4200/assets/',
 };
