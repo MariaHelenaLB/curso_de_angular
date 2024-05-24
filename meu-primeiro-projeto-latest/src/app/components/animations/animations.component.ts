959dfb5246051d7358dbf09d81702ed370127115
MariaHelenaLB <mariahelenalimabarreto13@gmail.com>
2024-05-24 09:35:06 -0300

    Aula 162 (Tansition + if).

--- meu-primeiro-projeto-latest/src/app/components/animations/animations.component.ts
+++ meu-primeiro-projeto-latest/src/app/components/animations/animations.component.ts
@@ -13,20 +13,24 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
     trigger('move-ball', [
 
       state('move-left', style({
+        opacity: 0,
         transform: 'scale(1) translateX(0) rotate(0deg)',
       })
       ),
 
       state('move-right', style({
+        opacity: 1,
         transform: 'scale(0.7) translateX(300px) rotate(360deg)',
       })
       ),
 
       //transition('move-left <=> move-right', animate('1s')),
-      transition('move-left <=> move-right', animate('1s')),
+      transition('move-left <=> move-right', animate('1s ease-int-out')),
+      transition(':enter', animate('1s')), //pode utilizar void => * também no lugar de :enter
+      transition(':leave', animate('2s')), //pode utilizar void => * também no lugar de :leave
       transition('* => move-right', animate('5s 1s ease-in-out')),
       transition('* => move-left', animate('1s')),
-
+      //'* => void' : significa que qualquer mudança de estado para "qualquer estado" para "vazio" (void) será animada usando a animação definida 
     ]),
   ],
 })
